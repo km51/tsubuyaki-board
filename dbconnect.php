91f70ee314243caee2250bf502c35910bc0b16cb
km51 <56459004+km51@users.noreply.github.com>
2021-01-18 06:00:46 +0900

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,7 @@
 <?php
 try{
 // 例外処理
-    $db = new PDO('mysql:dbname=heroku_587425a4d683686;port=3306;host=us-cdbr-east-03.cleardb.com;charset=utf8', 'b9ff9d69f410c3', 'c2846a98');
+    $db = new PDO('mysql:dbname=heroku_587425a4d683686;host=us-cdbr-east-03.cleardb.com;charset=utf8', 'b9ff9d69f410c3', 'c2846a98');
     // DBに接続できるようになった。
 
 }catch(PDOException $e){
