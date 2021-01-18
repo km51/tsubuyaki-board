95b61a2726a6d6960227200970c9170d641d6f31
km <busi.km623@gmail.com>
2021-01-18 17:20:37 +0900

    commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '93ce9e520338bf42741a805e138f59382c01143c',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '93ce9e520338bf42741a805e138f59382c01143c',
+    ),
+  ),
+);
